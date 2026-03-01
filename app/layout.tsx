77eae039f612c6f46ed9dfcb79335157120ad456
chenzhen <1583296383@qq.com>
2026-03-01 18:25:58 +0800

    侧边栏

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
+
 import "./globals.css";
 
-const inter = Inter({subsets:['latin'],variable:'--font-sans'});
+const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
